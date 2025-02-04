3300977400ea17f9a088e921f23837c9b97db7d3
nicemare61 <kittipoj.meer@bumail.net>
2025-02-04 22:28:07 +0700

    Adjust Code

---
+++ Assets/Player.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private bool micro;
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    
+    [Header("Movement")]
+    [SerializeField] int movementSpeed = 5;
+    [SerializeField] float xInput;
+    [SerializeField] float zInput;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            micro = !micro;
+        }
+        MoveBYKB();
+        MicroMacro();
+    }
+
+    void MoveBYKB()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+        
+        Vector3 dir = (transform.forward * zInput) + (transform.right * xInput);
+        transform.position += dir * movementSpeed * Time.deltaTime;
+        if (xInput < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (xInput > 0)
+        {
+            spriteRenderer.flipX = false;   
+        }
+    }
+
+    void MicroMacro()
+    {
+        if (micro)
+        {
+            playerTransform.transform.localScale = new Vector3(0.1f,0.1f,0.1f);
+        }
+        else
+        {
+            playerTransform.transform.localScale = new Vector3(0.3f,0.3f,0.3f);
+        }
+    }
+}
+
+
