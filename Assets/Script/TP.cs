b5e343ade8e395d60e18997961958575c357ee09
nicemare61 <kittipoj.meer@bumail.net>
2025-02-12 20:49:24 +0700

    CreateLadder

--- Assets/Script/TP.cs
+++ Assets/Script/TP.cs
@@ -1,10 +1,30 @@
 using System;
 using UnityEngine;
+using Vector3 = System.Numerics.Vector3;
 
+public enum Location
+{
+    Up,
+    Down
+}
 public class TP : MonoBehaviour
 {
+    [SerializeField] private GameObject playerPrefab;
     [SerializeField] private bool TestL;
     [SerializeField] private GameObject tpText;
+    [SerializeField] private Location location = Location.Down;
+
+    private void Update()
+    {
+        if(TestL && Input.GetKeyDown(KeyCode.E))
+        {
+            if (location == Location.Down)
+            {
+                playerPrefab.transform.position = playerPrefab.transform.position;
+            }
+        }
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.name == "Player")
